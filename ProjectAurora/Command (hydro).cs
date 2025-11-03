f07655f5dc9e3ef2bc6bae2996e8f3d1e1dce608
barna25 <bahut24@student.sdu.dk>
2025-11-03 13:48:29 +0100

    hydro files

---
+++ ProjectAurora/Command (hydro).cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldOfZuul
+{
+    public class Command
+    {
+        public string Name { get; }
+        public string? SecondWord { get; } // this might be used for future expansions, such as "take apple".
+
+        public bool HasSecondWord()
+        {
+            return SecondWord != null;
+        }
+
+        public Command(string name, string? secondWord = null)
+        {
+            Name = name;
+            SecondWord = secondWord;
+        }
+    }
+}
